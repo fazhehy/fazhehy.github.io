66521f482a4247c885537f31e7ab2e9314f837f5
fazhehy <3048973161@qq.com>
2025-10-27 18:19:22 +0800

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://fazhehy.github.io/",
-	base: "/fazhehy.github.io",
+	base: "/fazhehy.github.io/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
