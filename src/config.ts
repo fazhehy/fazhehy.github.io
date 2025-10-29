3811563f46057801e009874e32a2c55422ffe232
fazhehy <3048973161@qq.com>
2025-10-29 22:36:38 +0800

    update site

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,8 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "fazhehy's blog",
-	subtitle: "",
-	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	subtitle: "(^.^)",
+	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: true, // Hide the theme color picker for visitors
